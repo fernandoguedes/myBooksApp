b05db82bf20b39bb90273615ef2f23c826407bff
Luís Fernando Guedes <lfernandoguedes@gmail.com>
2017-09-02 16:47:37 -0300

    Implements pagination

--- app/components/search/search.controller.js
+++ app/components/search/search.controller.js
@@ -10,11 +10,28 @@
     function searchCtrl(bookService) {
         var self = this;
 
+        self.startIndex = 0;
+
         self.searchBook = function(keyword) {
-            bookService.search(keyword)
+            if (self.keyword !== keyword) {
+                self.keyword = keyword;
+                self.startIndex = 0;
+            }
+
+            bookService.search(self.keyword, self.startIndex)
                 .then(function(books) {
                     self.searchResults = books.data;
                 });
+        }
+
+        self.goNextPage = function(keyword) {
+            self.startIndex = self.startIndex + 40;
+            self.searchBook(keyword);
+        }
+
+        self.goPreviousPage = function(keyword) {
+            self.startIndex = self.startIndex - 40;
+            self.searchBook(keyword);
         }
     }
 

77fc064eb68ce49024f50f89d79f8cc6a22330b8
Luís Fernando Guedes <lfernandoguedes@gmail.com>
2017-09-02 16:48:10 -0300

    Changes book service to support pagination

--- app/shared/services/book.service.js
+++ app/shared/services/book.service.js
@@ -9,17 +9,23 @@
 
     function bookService($http) {
         var URL = 'https://www.googleapis.com/books/v1/volumes';
-        var KEY = '?key=AIzaSyD3d4rTM7BRgBoM2Ug23I6o5hAxnKuJpwA';
+        var KEY = 'key=AIzaSyD3d4rTM7BRgBoM2Ug23I6o5hAxnKuJpwA';
 
-        this.search = function(keyword) {
-            var apiUrl = URL + '?q=' + keyword;
+        this.search = function(keyword, startIndex) {
+            var apiUrl = URL + '?q={keyword}&startIndex={startIndex}&maxResults=40&{key}';
+            apiUrl = apiUrl
+                .replace('{keyword}', keyword)
+                .replace('{startIndex}', startIndex)
+                .replace('{key}', KEY);
+
             return $http.get(apiUrl);
         }
 
         this.getById = function(id) {
-            var apiUrl = URL + '/' + id + KEY;
+            var apiUrl = URL + '/{id}/?{key}';
+            apiUrl = apiUrl.replace('{id}', id).replace('{key}', KEY)
+
             return $http.get(apiUrl);
         }
-
     }
 })();
